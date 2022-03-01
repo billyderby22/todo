413dee51a8eab2c39ca043def9c50a20bcb8f67f
billyderby <billyderby97@gmail.com>
2022-03-01 15:04:27 +0000

    added application files

---
+++ application/routes.py
@@ -0,0 +1,48 @@
+from flask import redirect, url_for
+from application import app, db
+from application.models import Todo, Project
+from datetime import date, timedelta
+
+@app.route('/')
+def home():
+    return f"{Todo.query.count()} todos: " + '<br>'.join(str(t) + " " + str(t.project) for t in Todo.query.all())
+
+@app.route('/search=<keyword>')
+def search(keyword):
+    data = db.session.execute(f"SELECT * FROM todo WHERE desc LIKE '%{keyword}%'")
+    return '<br>'.join([str(res) for res in data])
+
+@app.route('/done')
+def done():
+    return '<br>'.join(str(t) for t in Todo.query.filter_by(status='done').order_by(Todo.title.desc()).all())
+
+@app.route('/create/<int:pnum>/<title>/<desc>')
+def create(pnum, title, desc):
+    todo = Todo(title=title, desc=desc, status='todo', proj_id = pnum)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/create-proj/<name>')
+def create_project(name):
+    new_proj = Project(project_name = name, due_date = date.today() + timedelta(30))
+    db.session.add(new_proj)
+    db.session.commit(),
+    return redirect(url_for('home'))
+
+@app.route('/update/<int:pk>/<newstatus>')
+@app.route('/update/<int:pk>/<newtitle>/<newstatus>')
+def update(pk, newstatus, newtitle = None): # defining multiple routes and setting default val in function def allows the user to either update just the status, or update the task description and status
+    todo = Todo.query.get(pk)
+    if newtitle:
+        todo.title = newtitle
+    todo.status = newstatus
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete/<int:i>')
+def delete(i):
+    todo = Todo.query.get(i)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
